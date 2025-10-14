411bad052edb3312bf386a7eeed9773cb1286511
muthu <muthu.subramanian@dhsit.co.uk>
2025-10-14 15:14:00 +0000

    supplier response repo update

--- repositories/supplier_response_repo.py
+++ repositories/supplier_response_repo.py
@@ -269,8 +269,7 @@ def lookup_workflow_for_unique(*, unique_id: str) -> Optional[str]:
             "SELECT workflow_id "
             "FROM proc.supplier_response "
             "WHERE unique_id=%s "
-            "ORDER BY id DESC "
-            "LIMIT 1"
+            "ORDER BY unique_id DESC "
         )
         cur.execute(q, (unique_id,))
         row = cur.fetchone()
